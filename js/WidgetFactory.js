8590414527402ebf879a900a025790fb741e781d
smiling-Jack <Steffen.schorling@googlemail.com>
2014-04-24 18:08:46 +0200

    Knob & Allgemein

--- js/WidgetFactory.js
+++ js/WidgetFactory.js
@@ -41,21 +41,18 @@ var WF = {
         });
 
 
-
-
         // WGS XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
         $("#wgs_general_head")
             .button()
-            .click(function(){
+            .click(function () {
 
-            $("#wgs_general_content").toggle();
+                $("#wgs_general_content").toggle();
                 $(this).removeClass('ui-state-focus')
-        });
+            });
 
 
         // Toolbox XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
 
 
         $(document).keydown(function (event) {
@@ -76,26 +73,71 @@ var WF = {
 
         WF.menu_iconbar();
         WF.context_menu();
+
+
+        WF.add_knob();
     },
 
-    add_knob: function(){
-
+    add_knob: function () {
         WF.add_new_widget();
         WF.add_widget_settings("knob")
 
+
+        var stroke_w = 100;
+        var start_winkel= 0;
+        var stop_winkel = 90;
+
+        var max =100
+        var min =0
+
+        var val = 100
+
+        var _turn_winkel = start_winkel * Math.PI / 180;
+
+        var x1 =  500 + 450 * Math.sin(_turn_winkel );
+        var y1 =  500 - 450 * Math.cos(_turn_winkel );
+
+        var winkel_max = (start_winkel-stop_winkel)*-1;
+        var winkel = winkel_max/(max-min)*val+start_winkel;
+
+        var x2 = 500 + 450 * Math.sin(winkel* Math.PI / 180 );
+        var y2 = 500 - 450 * Math.cos(winkel* Math.PI / 180 );
+
+        console.log(winkel)
+
+        $('#new_widget').append('<svg \
+           viewBox="0 0 1000 1000"\
+            width="100%" \
+            height="100%">\
+            <path name="hallo" stroke="blue" stroke-width='+stroke_w +' fill="none"/></svg>');
+
+        $('#new_widget_body').resizable({
+            aspectRatio: true,
+        })
+            .draggable();
+
+
+                if ((winkel * 180 / Math.PI)-start_winkel > 180  ) {
+
+                    $('[name="hallo"]').attr("d", "M" + x1 + "," + y1 + " A450,450 0 0,1 " + x2 + "," + y2 + "  ");
+                } else {
+                    $('[name="hallo"]').attr("d", "M" + x1 + "," + y1 + " A450,450 0 1,1 " + x2 + "," + y2 + "  ");
+                }
+
+
     },
 
-add_new_widget: function(){
+    add_new_widget: function () {
 
-    $("#prg_panel").append('<div id="new_widget" style="width: 300px; height: 300px ; border: 1px dashed red"> </div>')
-
-
-},
-
-    add_widget_settings: function(type){
+        $("#prg_panel").append('<div id="new_widget_body" style=" width500px; height:500px; border: 1px solid yellow"><div id="new_widget" style="width:100%; height: 100%;  border: 1px dashed red"> </div></div>')
 
 
-},
+    },
+
+    add_widget_settings: function (type) {
+
+
+    },
 
 
 };
@@ -110,8 +152,6 @@ add_new_widget: function(){
 //};
 
 
-
-
 (function () {
     $(document).ready(function () {
         // Lade Theme XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -121,7 +161,6 @@ add_new_widget: function(){
         }
         $("#theme_css").remove();
         $("head").append('<link id="theme_css" rel="stylesheet" href="css/' + WF.theme + '/jquery-ui.min.css"/>');
-
 
 
         // Lade ccu.io Daten XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
